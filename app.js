45d6f5ce4168599cdfeea57b09f832ed4b4783ae
eddybpro <abdelilahdb154@gmail.com>
2022-11-10 13:10:35 +0100

    add quiries

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 const full = document.getElementById('imagebox');
+const showUl = document.getElementById('humber')
+const closeUl = document.getElementById('close')
+const ulList = document.getElementById('list')
+const li_Item= ulList.querySelectorAll('li');
+
+showUl.addEventListener('click',()=>{
+    ulList.classList.remove('hidden')
+    ulList.classList.add('blok')
+    showUl.style.display = 'none';
+    closeUl.style.display = 'block';
+})
+
+closeUl.addEventListener('click',()=>{
+    ulList.classList.add('hidden')
+    ulList.classList.remove('blok')
+    showUl.style.display = 'block';
+    closeUl.style.display = 'none';
+})
+
+li_Item.forEach(li=>{
+    li.addEventListener('click',()=>{
+        ulList.classList.add('hidden')
+    ulList.classList.remove('blok')
+    showUl.style.display = 'block';
+    closeUl.style.display = 'none';
+    })
+})
 
 
 
